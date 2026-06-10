9c1ba55a88135fe874596034dc878856bdc21233
j4v1r <jcolungaa2000@alumno.ipn.mx>
2026-06-09 19:30:29 -0600

    Configuración 'frontend'

---
+++ frontend/webpack.config.js
@@ -0,0 +1,53 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/Application.jsx',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js',
+        clean: true
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            }
+        ]
+    },
+
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+
+    plugins: [ 
+        new HtmlWebpackPlugin({
+            template: './plantilla/index.html',
+            path: path.resolve(__dirname, 'dist'),
+            filename: 'index.html'
+        })
+    ],
+
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, 'dist'),
+        },
+        port: 8080, //Puerto del servidor
+        historyApiFallback: true, //Activa el modo de historial para aplicaciones de una sola página
+        open: true, //Abre el navegador automáticamente al iniciar el servidor
+        hot: true, //Habilita el reemplazo en caliente de módulos (Hot Module Replacement)
+    }
+};
