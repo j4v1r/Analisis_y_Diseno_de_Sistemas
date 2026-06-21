1affabd2534b4736b7395ef201c2423471ab20ad
j4v1r <jcolungaa2000@alumno.ipn.mx>
2026-06-20 20:25:36 -0600

    PROYECTO CASI FINAL

--- backend/src/main/java/API/EliminarDiagrama.java
+++ backend/src/main/java/API/EliminarDiagrama.java
@@ -17,16 +17,19 @@ import org.json.JSONObject;
  * @author aleja
  */
 public class EliminarDiagrama extends HttpServlet {
-    
+
     protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    
+
         response.setContentType("application/json");
         response.setCharacterEncoding("UTF-8");
-        
+        response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+        response.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+        response.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
         JSONArray arreglo = new JSONArray();
-        
-        try(PrintWriter out = response.getWriter()) {
-            
+
+        try ( PrintWriter out = response.getWriter()) {
+
             DB bd = new DB();
             bd.setConnection(
                     "com.mysql.cj.jdbc.Driver",
@@ -34,21 +37,21 @@ public class EliminarDiagrama extends HttpServlet {
             );
 
             int id_diagrama = Integer.parseInt(request.getParameter("id"));
-            
+
             PreparedStatement ps = bd.getConnection().prepareStatement(
                     "DELETE FROM diagrama WHERE id_diagrama=?"
             );
-            
+
             ps.setInt(1, id_diagrama);
 
             int filas = ps.executeUpdate();
-            
-            if(filas>0){
+
+            if (filas > 0) {
                 out.print("{\"status\":\"yes\"}");
-            }else{
+            } else {
                 out.print("{\"status\":\"no\"}");
             }
-            
+
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -64,5 +67,16 @@ public class EliminarDiagrama extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
+    }
+
+    @Override
+    protected void doOptions(HttpServletRequest request,
+            HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+        response.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+        response.setHeader("Access-Control-Allow-Headers", "Content-Type");
+        response.setStatus(HttpServletResponse.SC_OK);
     }
 }
